57c7cfb68421db1d16200e9aa90d200f3c71d86b
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-23 14:05:33 -0800

    refractored play.js but importing setupInput function from input.js

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 const { stdin } = require('process');
 const connect = require('./client');
+const setupInput = require('./input');
 
 console.log("Connecting ...");
 connect().on('data', (data) => {
   console.log(data);
 });
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
