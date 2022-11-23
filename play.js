488edad95e8b48d5ee523a596f9402843ab6aa65
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-23 13:58:37 -0800

    add and event that will exit the game when input ctr + c

--- play.js
+++ play.js
@@ -1,6 +1,24 @@
+const { stdin } = require('process');
 const connect = require('./client');
 
 console.log("Connecting ...");
 connect().on('data', (data) => {
   console.log(data);
 });
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
